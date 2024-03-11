d552e30ab5caedf68c5ce6d15795fc510435c132
sebastianbiju05 <92808534+sebastianbiju05@users.noreply.github.com>
2024-03-11 22:02:30 +0530

    Update dict.5.py

--- dictionary/dict.5.py
+++ dictionary/dict.5.py
@@ -1,22 +1,24 @@
-d={}
 l={}
 f=True
+l1=[]
+l2=[]
+l3=[]
 while (f==True):
     s=input("Enter team or stop :")
     if s=='stop':
         f==False
-        break   
-    n=int(input("enter no. of games won"))
-    n2=int(input("enter no. of games lost"))
-    d[s]=n
-    l[s]=n2
-max=0
-min=0
-for key in d:
+        break 
+    n=int(input("enter no. of games won:"))
+    n2=int(input("enter no. of games lost:"))
+    l1.append(n)      
+    l2.append(n2)
+    l3.append(s)
+    l[s]=[n,n2]    
+for key in l:
     print(key)
-    if max<d[s]:
-        max=d[s]
-    if min>l[s]:
-        min=l[s]
-print(max)
-print(min)
+print('team with greatest wins:',l3[l1.index(max(l1))])
+print('team with greatest loss:',l3[l2.index(max(l2))])
+s=input("Enter team to find win percentage")
+l4=l[s]
+print(l4)
+print('win percentage:',float((l4[0])/(l4[0]+l4[1]))*100)
